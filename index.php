eb6719c32a7b9a6afd6e7cfc802b8abd078a216b
Saina Ghasemian <sainaghasemian@gmail.com>
2023-02-13 12:08:51 -0700

    Tester for main page
    Use :http://localhost/SENG401-Huddle/
    to get to main page.
    This is just to see if main page works

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+    session_start();
+?>
+
+<html>
+<style type="text/css">
+	body{
+		text-align: middle;
+		padding-bottom: 100px;
+		background: #F5F5F5;
+		padding-top: 120px;
+		font-size: 20px;
+		color: #C5A586;
+	}
+	header{
+		margin-top: -120;
+  		padding: -50px;
+		font-size: 50px;
+		font-weight: 50;
+		background-image: linear-gradient(to left, #F5F5F5, #b393d3);
+		color: transparent;
+		background-clip: text;
+		-webkit-background-clip: text;
+	}
+	button{
+		font-size: 30px;
+		font-weight: 90;
+		background-color: #f7e6d9;
+    	box-shadow: 0 0 0 2px #DEB8A0;
+		transition: all .9s ease;
+		align: top;
+	}
+	img{
+		align: middle;
+	}
+</style>
+
+<header>
+	<h1> Huddle </h1>
+
+</header>
+
+
+<body>
+
+<table style="vertical-align: top; margin-left: auto; margin-right: auto;">
+	<tr>
+	<tr>
+
+</body>
+</html>
