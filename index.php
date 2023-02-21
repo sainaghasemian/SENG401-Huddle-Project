7bb139a46bad5a477b1d751e9f8d2031442d9eef
Saina Ghasemian <sainaghasemian@gmail.com>
2023-02-21 11:06:49 -0700

    Connection to database correctly

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
     session_start();
-
-    $con=mysqli_connect("localhost","root","","huddle");
-    // Check connection
-    if (mysqli_connect_errno())
-    {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
 ?>
 
 <!DOCTYPE html>
