4baf5d4c0b927d4a218aeb3f9d61d18a3baab917
ilemieux8 <101215683+ilemieux8@users.noreply.github.com>
2023-03-27 17:54:44 -0600

    Users can now follow new teams

---
+++ add-new-teams.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+
+    // Include the database connection file
+    include_once("config.php");
+
+    $team = $_POST['team'];
+
+    if ($team == ""){
+        $_SESSION["message"] .= " A team must be selected $team.";
+        header("Location: index.php");
+        exit;
+    }
+
+    $teamID;
+
+    if ($team == "Anaheim Ducks"){ $teamID = 670; }
+    else if ($team == "Arizona Coyotes"){ $teamID = 1460; }
+    else if ($team == "Boston Bruins"){ $teamID = 673; }
+    else if ($team == "Buffalo Sabres"){ $teamID = 674; }
+    else if ($team == "Calgary Flames"){ $teamID = 675; }
+    else if ($team == "Carolina Hurricanes"){ $teamID = 676; }
+    else if ($team == "Chicago Blackhawks"){ $teamID = 678; }
+    else if ($team == "Colorado Avalanche"){ $teamID = 679; }
+    else if ($team == "Columbus Blue Jackets"){ $teamID = 680; }
+    else if ($team == "Dallas Stars"){ $teamID = 681; }
+    else if ($team == "Detroit Red Wings"){ $teamID = 682; }
+    else if ($team == "Edmonton Oilers"){ $teamID = 683; }
+    else if ($team == "Florida Panthers"){ $teamID = 684; }
+    else if ($team == "Los Angeles Kings"){ $teamID = 685; }
+    else if ($team == "Minnesota Wild"){ $teamID = 687; }
+    else if ($team == "Montreal Canadiens"){ $teamID = 688; }
+    else if ($team == "Nashville Predators"){ $teamID = 689; }
+    else if ($team == "New Jersey Devils"){ $teamID = 690; }
+    else if ($team == "New York Islanders"){ $teamID = 691; }
+    else if ($team == "New York Rangers"){ $teamID = 692; }
+    else if ($team == "Ottawa Senators"){ $teamID = 693; }
+    else if ($team == "Philadelphia Flyers"){ $teamID = 695; }
+    else if ($team == "Pittsburgh Penguins"){ $teamID = 696; }
+    else if ($team == "San Jose Sharks"){ $teamID = 697; }
+    else if ($team == "Seattle Kraken"){ $teamID = 1436; }
+    else if ($team == "St Louis Blues"){ $teamID = 698; }
+    else if ($team == "Tampa Bay Lightning"){ $teamID = 699; }
+    else if ($team == "Toronto Maple Leafs"){ $teamID = 700; }
+    else if ($team == "Vancouver Canucks"){ $teamID = 701; }
+    else if ($team == "Vegas Golden Knights"){ $teamID = 702; }
+    else if ($team == "Washington Capitals"){ $teamID = 703; }
+    else if ($team == "Winnipeg Jets"){ $teamID = 704; }
+    
+    $result = $pdo->query("SELECT * FROM usersubscription WHERE Team_TeamID = $teamID AND User_UserID ='{$_SESSION["authenticated_username"]}'");
+    $success = $result->fetch(PDO::FETCH_ASSOC);
+    if($success != null)
+    {   
+        $_SESSION["message"] = "You already subscribe to the $team.";
+    }
+    else{
+        $pdo->query("INSERT INTO `usersubscription` (`User_UserID`, `Team_TeamID`) VALUES
+        ('{$_SESSION["authenticated_username"]}', $teamID);");
+    }
+    header("Location: index.php");
+?>
--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     // Include the database connection file
     include_once("config.php");
 
+    echo $_SESSION["message"];
+    $_SESSION["message"] = "";
+
     //Verify username and password from login page
     if (count($_POST) && isset($_POST["username"]) && isset($_POST["password"]))
     {
@@ -107,7 +110,7 @@
                 else
                 {
                   $loggedInUser = $_SESSION["authenticated_username"];
-                  $result = $pdo->query("SELECT * FROM team JOIN usersubscription ON team.teamID = usersubscription.Team_TeamID JOIN post ON usersubscription.Team_TeamID = post.Team_TeamID WHERE usersubscription.User_UserID = '$loggedInUser';");
+                  $result = $pdo->query("SELECT * FROM team JOIN usersubscription ON team.teamID = usersubscription.Team_TeamID JOIN post ON usersubscription.Team_TeamID = post.Team_TeamID WHERE usersubscription.User_UserID = '$loggedInUser' ORDER BY post.DatePosted DESC;");
                 }
 
                 $posts = $result->fetchAll(PDO::FETCH_DEFAULT);
@@ -202,10 +205,47 @@
               ?>
                 <span class="index-text04"><span>My Feed</span></span>
                 <span class="index-text06"><span>My Teams</span></span>
-                <div class="index-add-new-teams-button-container">
-                  <a href="add-new-teams.php" class="index-add-new-teams-button">Add New Teams</a>
-                </div>
-
+                <form id = post action = "add-new-teams.php" method="post">
+                  <select class = "index-page-teams" name = "team">
+                    <option value="">Add Team</option>
+                    <option value="Anaheim Ducks">Anaheim Ducks</option>
+                    <option value="Arizona Coyotes">Arizona Coyotes</option>
+                    <option value="Boston Bruins">Boston Bruins</option>
+                    <option value="Buffalo Sabres">Buffalo Sabres</option>
+                    <option value="Calgary Flames">Calgary Flames</option>
+                    <option value="Carolina Hurricanes">Carolina Hurricanes</option>
+                    <option value="Chicago Blackhawks">Chicago Blackhawks</option>
+                    <option value="Colorado Avalanche">Colorado Avalanche</option>
+                    <option value="Columbus Blue Jackets">Columbus Blue Jackets</option>
+                    <option value="Dallas Stars">Dallas Stars</option>
+                    <option value="Detroit Red Wings">Detroit Red Wings</option>
+                    <option value="Edmonton Oilers">Edmonton Oilers</option>
+                    <option value="Florida Panthers">Florida Panthers</option>
+                    <option value="Los Angeles Kings">Los Angeles Kings</option>
+                    <option value="Minnesota Wild">Minnesota Wild</option>
+                    <option value="Montreal Canadiens">Montreal Canadiens</option>
+                    <option value="Nashville Predators">Nashville Predators</option>
+                    <option value="New Jersey Devils">New Jersey Devils</option>
+                    <option value="New York Islanders">New York Islanders</option>
+                    <option value="New York Rangers">New York Rangers</option>
+                    <option value="Ottawa Senators">Ottawa Senators</option>
+                    <option value="Philadelphia Flyers">Philadelphia Flyers</option>
+                    <option value="Pittsburgh Penguins">Pittsburgh Penguins</option>
+                    <option value="San Jose Sharks">San Jose Sharks</option>
+                    <option value="Seattle Kraken">Seattle Kraken</option>
+                    <option value="St. Louis Blues">St. Louis Blues</option>
+                    <option value="Tampa Bay Lightning">Tampa Bay Lightning</option>
+                    <option value="Toronto Maple Leafs">Toronto Maple Leafs</option>
+                    <option value="Vancouver Canucks">Vancouver Canucks</option>
+                    <option value="Vegas Golden Knights">Vegas Golden Knights</option>
+                    <option value="Washington Capitals">Washington Capitals</option>
+                    <option value="Winnipeg Jets">Winnipeg Jets</option>
+                  </select>
+                    <div class="index-add-new-teams-button-container">
+                      <button type="submit" class="index-add-new-teams-button">Add</button>
+                    </div>
+                  
+                </form>
               <?php
               }
               ?>
