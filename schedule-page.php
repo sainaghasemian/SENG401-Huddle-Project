779a154e66055a4b4df6aa5de7969da718817954
Saina Ghasemian <sainaghasemian@gmail.com>
2023-03-27 11:22:44 -0600

    schedule change

--- schedule-page.php
+++ schedule-page.php
@@ -138,7 +138,6 @@
           class="new-team-page-rectangle12"
         />
         <span onclick="getGame()" class="schedule-page-get-games-buttontext"><span>Get Games</span></span>
-          
       </button>
       
 
