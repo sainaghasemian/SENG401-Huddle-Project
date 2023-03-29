6680495d6fb497282e810d524eddabf6ed5b52d7
tommydinh13 <81772838+tommydinh13@users.noreply.github.com>
2023-03-28 22:39:45 -0600

    getStandings() now full league
    used to be western league then eastern but now its all of NHL

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ function getStandings(userTeam) {
             // western conference loop
             if (selectedTeam === "") {
 
-                for (let i = 0; i < 32; i++) {
+                for (let i = 32; i < 64; i++) {
                     teamLogo = objectData.response[0][i].team.logo;
                     teamName = objectData.response[0][i].team.name;
                     gamesPlayed = objectData.response[0][i].games.played;
@@ -903,5 +903,4 @@ function homePageGameScheduleLoggedIn(teamArr) {
 
 
 }
-
 
